2a924d1b7db08bac89deb8735706e5360b9bf16a
emelnik2 <emelnik@ametec.com.mx>
2023-04-17 12:06:38 -0600

    Update StoredXSS_Fixed.java

--- StoredXSS_Fixed.java
+++ StoredXSS_Fixed.java
@@ -21,9 +21,9 @@ public class DomXssMitigationServlet extends HttpServlet {
             int read;
             do{
                 read = input.read(byteBuf, 0, byteBuf.length);
-                String escapedRead = StringEscapeUtils.escapeHtml4(name);
+                String escapedRead = StringEscapeUtils.escapeHtml4(read);
                 if(read > 0) {
-                    outputStream.write(byteBuf, 0, read);
+                    outputStream.write(byteBuf, 0, escapedRead);
                 }
             }while (read >=0);
             input.close();
